fc78ce9f6e2e291f71e351b85e2b1cc1f203ae74
Unknown <bushmango@gmail.com>
2020-05-11 15:42:54 -0500

    basic hex experiments

--- components/hex/Hex.tsx
+++ components/hex/Hex.tsx
@@ -32,6 +32,11 @@ export const Hex = () => {
   let h = 400
 
   React.useEffect(() => {
+    loadImage('coffee', '/images/pixabay/coffee-1030971_640.jpg')
+    loadImage('compass', '/images/pixabay/compass-5137269_640.jpg')
+  })
+
+  React.useEffect(() => {
     if (refCanvas.current) {
       render(refCanvas.current, elapsed, w, h)
     }
@@ -50,6 +55,36 @@ export const Hex = () => {
       </div>
     </div>
   )
+}
+
+interface IImage {
+  key: string
+  src: string
+  loaded: boolean
+  image: HTMLImageElement
+}
+const images: { [key: string]: IImage } = {}
+
+const loadImage = (key: string, src: string) => {
+  if (!images[key]) {
+    let img = new Image() // Create new img element
+    images[key] = {
+      key,
+      src,
+      loaded: false,
+      image: img,
+    }
+    img.addEventListener(
+      'load',
+      () => {
+        // execute drawImage statements here
+        images[key].image = img
+        images[key].loaded = true
+      },
+      false,
+    )
+    img.src = src // Set source path
+  }
 }
 
 export const render = (
@@ -78,8 +113,21 @@ export const render = (
   // hex(ctx, 50 + hr, 50 + hh, r)
   // hex(ctx, 50 + hr * 2, 50, r)
 
+  if (images['coffee']?.loaded) {
+    ctx.drawImage(images['coffee'].image, 50, 50)
+  }
+
   for (let i = 0; i < 5; i++) {
     hexRow(ctx, 50, 50 + i * hh * 2, r, 5)
+  }
+
+  if (images['compass']?.loaded) {
+    for (let i = 0; i < 3; i++) {
+      ctx.save()
+      clipHex(ctx, 50, 50 + hh * 2 * i, r)
+      ctx.drawImage(images['compass'].image, 0, 0)
+      ctx.restore()
+    }
   }
 }
 
@@ -106,6 +154,31 @@ const hexRow = (
     }
     hex(ctx, x + hr * i, y + oy, r)
   }
+}
+
+const clipHex = (
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  r: number,
+) => {
+  ctx.beginPath()
+
+  let points: Array<[number, number]> = []
+  points.push([-r, 0])
+  points.push(rotate2(0, 0, -r, 0, turn / 6))
+  points.push(rotate2(0, 0, -r, 0, (2 * turn) / 6))
+  points.push([r, 0])
+  points.push(rotate2(0, 0, -r, 0, (4 * turn) / 6))
+  points.push(rotate2(0, 0, -r, 0, (5 * turn) / 6))
+  //ctx.translate(x, y)
+
+  _.forEach(points, (c) => {
+    ctx.lineTo(c[0] + x, c[1] + y)
+  })
+
+  ctx.closePath()
+  ctx.clip()
 }
 
 const hex = (
