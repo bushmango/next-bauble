e1420d085c320727a97a4dc9bf67f3215108150b
Unknown <bushmango@gmail.com>
2021-08-31 12:32:06 -0500

    update spokes and colors to clock

--- components/clocks/ClockPennyFarthing.tsx
+++ components/clocks/ClockPennyFarthing.tsx
@@ -7,16 +7,10 @@ import { clockUtil } from '../trash-compactor/clocks/clockUtil-sidecar'
 import { Graph } from '../trash-compactor/graphs/Graph'
 import { Translate } from '../trash-compactor/graphs/Translate'
 
-const subSecondTicks = 10
-const numTicks = 60 * subSecondTicks
-
 const numTicksGeneral = 60 * 10
 
 const numTicksSecondsInMinute = numTicksGeneral
-const numTicksMinutesInHour = numTicksGeneral
 const numTicksHourInDay = numTicksGeneral
-const numTicksDayInMonth = numTicksGeneral
-const numTicksMonthInYear = numTicksGeneral
 
 function _ticksHourInHalfDay() {
   return clockUtil.ticksHourInHalfDay(numTicksHourInDay)
@@ -42,11 +36,6 @@ export const ClockPennyFarthingFull = () => {
 }
 
 export const ClockPennyFarthing = () => {
-  let daysInMonth = clockUtil.daysInMonth()
-  let labelsMonth = clockUtil.monthLabels(false)
-  let labelsDates = clockUtil.dateLabels(false)
-  let hourLabels = clockUtil.hourLabels()
-
   let innerSpacing = 50
   let spacing = 35
   let maxSize = (innerSpacing + spacing * 4) * 2 + 40
@@ -55,9 +44,6 @@ export const ClockPennyFarthing = () => {
     <div>
       <div>
         <div style={{ position: 'relative', minHeight: '400px' }}>
-          <div style={{ position: 'absolute' }}>
-            <img src='/svgs/penny-farthing-clock.svg' />
-          </div>
           <div style={{ position: 'absolute' }}>
             <Graph width={maxSize} height={maxSize} fill=''>
               <Translate x={155} y={190}>
@@ -70,8 +56,9 @@ export const ClockPennyFarthing = () => {
                   }))}
                   x={0}
                   y={0}
-                  r={110}
+                  r={135}
                   fontSize={20}
+                  textAdjust={-20}
                   updateIntervalSeconds={0.1}
                   maxTicks={numTicksGeneral}
                   maxLabel={12}
@@ -89,7 +76,8 @@ export const ClockPennyFarthing = () => {
                   rotOffset={-23}
                   x={0}
                   y={0}
-                  r={30}
+                  r={40}
+                  textAdjust={-12}
                   fontSize={10}
                   updateIntervalSeconds={0.1}
                   maxTicks={numTicksGeneral}
@@ -98,6 +86,9 @@ export const ClockPennyFarthing = () => {
                 />
               </Translate>
             </Graph>
+          </div>
+          <div style={{ position: 'absolute' }}>
+            <img src='/svgs/penny-farthing-clock.svg' />
           </div>
         </div>
       </div>
@@ -115,6 +106,7 @@ export const Rotator = (props: {
   x: number
   y: number
   r: number
+  textAdjust: number
   fontSize: number
   updateIntervalSeconds: number
   maxTicks: number
@@ -142,6 +134,11 @@ export const Rotator = (props: {
 
   return (
     <Translate x={x} y={y}>
+      <circle cx={0} cy={0} fill='#777' r={props.r * 1} />
+      <circle cx={0} cy={0} fill='#BFBFBF' r={props.r * 0.95} />
+
+      {/* <path stroke={'green'} fill='white' d='M 100 100 A 50 50 0 0 0 0 0' /> */}
+
       <g transform={`rotate(${-rot * 360 + props.rotOffset})`}>
         {l.map(labels, (c) => (
           <g
@@ -150,20 +147,27 @@ export const Rotator = (props: {
           >
             <Translate x={0} y={r}>
               <text
-                dy={6}
+                dy={props.textAdjust}
                 fontSize={props.fontSize}
                 alignmentBaseline='middle'
                 textAnchor='middle'
               >
                 {'' + c.label}
               </text>
-              <circle cx={0} cy={-6} fill='blue' r={2} />
+              <circle cx={0} cy={-props.fontSize / 2} fill='#333' r={2} />
+              <line
+                x1={0}
+                y1={-r * 0.05 + props.textAdjust * 1.3}
+                x2={0}
+                y2={-r * 0.9}
+                stroke={'#555'}
+              />
             </Translate>
           </g>
         ))}
 
         <Translate x={0} y={posY}>
-          <circle cx={0} cy={0} fill='blue' r={3} />
+          <circle cx={0} cy={0} fill='#333' r={3} />
         </Translate>
       </g>
     </Translate>
